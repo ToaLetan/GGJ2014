7b04352cfe72c0477bccbe8a3c3fdeefd907b279
Tyler Norman <letan881@hotmail.com>
2014-01-25 16:38:44 -0500

    Camera
    Added camera to follow player movement.

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour 
+{
+	//Exposing CamTarget to be changed in Unity at runtime.
+	public GameObject CamTarget = null;
+
+	private Vector3 newCamPos = Vector3.zero;
+
+	// Use this for initialization
+	void Start () 
+	{
+		newCamPos = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		newCamPos = new Vector3(CamTarget.transform.position.x, CamTarget.transform.position.y, newCamPos.z);
+
+		gameObject.transform.position = newCamPos;
+	}
+}
